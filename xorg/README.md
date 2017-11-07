0de68acd639033ef332084ecfeb51f01b0e11eb8
Michele Dell'Ambrogio <m@nextcode.ch>
2017-11-07 22:20:56 +0100

    external monitor

---
+++ xorg/README.md
@@ -0,0 +1,10 @@
+
+to set two monitors
+```
+xrandr --output eDP1 --auto --output DP1 --auto --left-of eDP1
+```
+
+to disable laptop LCD
+```
+xrandr --output eDP1 --off
+```
---
+++ xorg/home.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+
+# disable laptop monitor
+xrandr --output eDP1 --off
+
+xrandr --dpi 120
+
+# Load a resource file, and merge with the current settings
+xrdb -merge $DIR/XresourcesExternalMonitor
+
+# restat i3
+i3-msg restart
